8ad302ec01e5495545c88c21766d4fe7ae978c5c
detunized <detunized@gmail.com>
2012-04-09 17:52:22 +0200

    Cleanup

--- Classes/LostPassAppDelegate.h
+++ Classes/LostPassAppDelegate.h
@@ -1,21 +1,8 @@
-//
-//  LostPassAppDelegate.h
-//  LostPass
-//
-//  Created by detunized on 4/7/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-@interface LostPassAppDelegate : NSObject <UIApplicationDelegate> {
-    
-    UIWindow *window;
-    UINavigationController *navigationController;
+@interface LostPassAppDelegate: NSObject<UIApplicationDelegate>
+{
 }
 
-@property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+@property(nonatomic, retain) IBOutlet UIWindow *window;
+@property(nonatomic, retain) IBOutlet UINavigationController *navigationController;
 
 @end
-
