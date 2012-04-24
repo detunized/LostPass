1fb38d355f17ab495d84affa2ef2724dd4d62c60
detunized <detunized@gmail.com>
2012-04-24 19:53:32 +0200

    Login screen embryo

---
+++ Classes/LoginViewController.h
@@ -0,0 +1,5 @@
+@interface LoginViewController: UIViewController
+{
+}
+
+@end
---
+++ Classes/LoginViewController.mm
@@ -0,0 +1,13 @@
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+@end
