e2e9b28795081568528de30ac451222a7cbb1761
detunized <detunized@gmail.com>
2012-06-08 09:45:38 +0200

    Hide all screens when the app gets inactive (should take care of lock/unlock situation)

--- Classes/LostPassAppDelegate.mm
+++ Classes/LostPassAppDelegate.mm
@@ -241,23 +241,21 @@ BOOL databaseLoaded_ = NO;
 	
 	self.modalScreens = [NSMutableArray arrayWithCapacity:4];
 	
-	[self pushScreens];
-	
 	return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+	NSLog(@"applicationDidBecomeActive");
+
+	[self popAllScreens];
+	[self hideBlackScreen];
+	[self pushScreens];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
 	NSLog(@"applicationWillResignActive");
-	/*
-	 Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	 Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-	 */
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-	NSLog(@"applicationDidEnterBackground");
 
 	[self popAllScreens];
 	[self hideSmokeScreen];
@@ -267,34 +265,20 @@ BOOL databaseLoaded_ = NO;
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
 	NSLog(@"applicationWillEnterForeground");
-
-	[self popAllScreens];
-	[self hideBlackScreen];
-	[self pushScreens];
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
+- (void)applicationDidEnterBackground:(UIApplication *)application
 {
-	NSLog(@"applicationDidBecomeActive");
-	/*
-	 Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-	 */
+	NSLog(@"applicationDidEnterBackground");
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
 	NSLog(@"applicationWillTerminate");
-	/*
-	 Called when the application is about to terminate.
-	 See also applicationDidEnterBackground:.
-	 */
 }
 
 - (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
 {
-	/*
-	 Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
-	 */
 }
 
 - (void)dealloc
