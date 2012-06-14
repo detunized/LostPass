d4c0ab53fdb5a5109301d40f3e1ecf0f6efca71b
detunized <detunized@gmail.com>
2012-06-15 00:42:50 +0200

    Functions to convert C/C++ string to NS and back

--- Classes/Utilities.h
+++ Classes/Utilities.h
@@ -9,6 +9,26 @@ void enableApplicationInput();
 
 void callAfter(NSTimeInterval seconds, void (^block)());
 
+inline char const *toC(NSString *nsString)
+{
+	return [nsString UTF8String];
+}
+
+inline std::string toStd(NSString *nsString)
+{
+	return toC(nsString);
+}
+
+inline NSString *toNs(char const *cString)
+{
+	return [NSString stringWithUTF8String:cString];
+}
+
+inline NSString *toNs(std::string const& stdString)
+{
+	return toNs(stdString.c_str());
+}
+
 std::ostream &operator <<(std::ostream &stream, CGPoint const &point);
 std::ostream &operator <<(std::ostream &stream, CGSize const &size);
 std::ostream &operator <<(std::ostream &stream, CGRect const &rect);
